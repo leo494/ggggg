41a1f64ab9c67b70155239b2aecbc1579e529d12
LeeSeungYoon <tmddls0lek@gmail.com>
2023-09-07 20:50:03 +0900

    4th commit

--- cpp_1.cpp
+++ cpp_1.cpp
@@ -1,8 +1,15 @@
 #include <iostream>
+#include <stdio.h>
 
 using namespace std;
 int main (int argc, char* argv[]){
- 
+
+for (int i = 0; i < 8; i++)
+{
+    printf("*");
+    /* code */
+}
+
 /*화ㅏㅏㅏㅏㅏㅏㅏ*/
 /*마스터*/
     return 0;
